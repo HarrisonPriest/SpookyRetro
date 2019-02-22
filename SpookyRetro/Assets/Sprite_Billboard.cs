1fe8a7d353efe2fbf62cd33468536e551a018908
HarrisonPriest <hdprie7063@ung.edu>
2019-02-22 10:15:42 -0500

    Added terrain and implemented the street lamp

---
+++ SpookyRetro/Assets/Sprite_Billboard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprite_Billboard : MonoBehaviour {
+    GameObject c;
+    private void Start()
+    {
+        c =  GameObject.Find("PlayerCamera");
+    }
+    // Update is called once per frame
+    void Update () {
+        transform.LookAt(c.transform.position);
+    }
+}
